a113ea20fea837f1da0b01a16a163c75a1712f45
Maxim Buzdalov <mbuzdalov@gmail.com>
2023-11-13 02:40:10 +0000

    Add a torture test for LinLogSpace.

--- src/test/java/com/github/mbuzdalov/tree4network/rmq/LinLogSpaceRMQTests.java
+++ src/test/java/com/github/mbuzdalov/tree4network/rmq/LinLogSpaceRMQTests.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class LinLogSpaceRMQTests {
     private boolean next(int[] array) {
@@ -38,6 +39,35 @@ public class LinLogSpaceRMQTests {
                     }
                 }
             } while (next(array));
+        }
+    }
+
+    @Test
+    public void torture() {
+        Random random = new Random(141353423);
+        int maxSize = 50;
+        int[] array = new int[maxSize];
+        NaiveRMQ naive = new NaiveRMQ(array);
+        LinLogSpaceRMQ log = new LinLogSpaceRMQ(array);
+        for (int run = 0; run < 1000; ++run) {
+            int size = run == 0 ? maxSize : 2 + random.nextInt(maxSize - 1);
+            for (int i = 0; i < size; ++i) {
+                array[i] = random.nextInt(10000);
+            }
+            naive.reloadArray(size);
+            log.reloadArray(size);
+            for (int l = 0; l < size; ++l) {
+                for (int r = l + 1; r <= size; ++r) {
+                    int ei = naive.minimumIndex(l, r);
+                    int fi = log.minimumIndex(l, r);
+                    int ev = array[ei];
+                    int fv = array[fi];
+                    if (ev != fv) {
+                        Assert.fail("Array: " + Arrays.toString(array) + ", l = " + l + ", r = " + r
+                                + ", expected " + ei + " => " + ev + ", found " + fi + " => " + fv);
+                    }
+                }
+            }
         }
     }
 }
