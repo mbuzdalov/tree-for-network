e04b2f9ec15927642aadc1cd761be1567b76a43c
Maxim Buzdalov <mbuzdalov@gmail.com>
2024-01-18 04:18:28 +0000

    Speed up edge switch asymptotically.

--- src/main/java/com/github/mbuzdalov/tree4network/mut/EdgeSwitchMutation.java
+++ src/main/java/com/github/mbuzdalov/tree4network/mut/EdgeSwitchMutation.java
@@ -8,6 +8,7 @@ import com.github.mbuzdalov.tree4network.util.Combinatorics;
 import com.github.mbuzdalov.tree4network.util.Edge;
 import com.github.mbuzdalov.tree4network.util.Graphs;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public final class EdgeSwitchMutation implements Mutation<EdgeSwitchMutation.Context> {
@@ -54,6 +55,10 @@ public final class EdgeSwitchMutation implements Mutation<EdgeSwitchMutation.Con
         int v1 = flippedEdgeV.v1();
         int v2 = flippedEdgeV.v2();
 
+        tree.removeEdge(v1, v2);
+        context.markReachable(tree, v1);
+        tree.addEdge(v1, v2);
+
         // Remove other edges incident to v1
         int[] v1Other = new int[2];
         int n1Other = 0;
@@ -88,16 +93,65 @@ public final class EdgeSwitchMutation implements Mutation<EdgeSwitchMutation.Con
             tree.addEdge(v1, v2Other[i]);
         }
 
+        long newCost = result.cost();
+        int wDeg1 = weights.degree(v1);
+        for (int i = 0; i < wDeg1; ++i) {
+            int u1 = weights.getDestination(v1, i);
+            if (u1 != v2) {
+                int w = weights.getWeight(v1, i);
+                if (context.isMarked(u1)) {
+                    newCost += w;
+                } else {
+                    newCost -= w;
+                }
+            }
+        }
+        int wDeg2 = weights.degree(v2);
+        for (int i = 0; i < wDeg2; ++i) {
+            int u2 = weights.getDestination(v2, i);
+            if (u2 != v1) {
+                int w = weights.getWeight(v2, i);
+                if (context.isMarked(u2)) {
+                    newCost -= w;
+                } else {
+                    newCost += w;
+                }
+            }
+        }
+
         // Okay, this is our new tree
-        return new BestTreeAlgorithm.Result(costAlgo.compute(weights, tree), tree);
+        return new BestTreeAlgorithm.Result(newCost, tree);
     }
 
     public static class Context {
         private Context(int n) {
             mutations = new int[n - 1];
+            visited = new boolean[n];
         }
         private final int[] mutations;
+        private final boolean[] visited;
         private int used;
+
+        private void markReachable(BoundedForest tree, int v) {
+            Arrays.fill(visited, false);
+            visited[v] = true;
+            dfs(tree, v);
+        }
+
+        private boolean isMarked(int v) {
+            return visited[v];
+        }
+
+        private void dfs(BoundedForest tree, int v) {
+            int nAdj = tree.degree(v);
+            for (int i = 0; i < nAdj; ++i) {
+                int next = tree.getDestination(v, i);
+                if (!visited[next]) {
+                    visited[next] = true;
+                    dfs(tree, next);
+                }
+            }
+        }
 
         private int getMutation(Random random) {
             int firstUsed = mutations.length - used;
