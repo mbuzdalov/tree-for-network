e0f57b76a8891d7b7985c68f027edf7b3c949a48
Maxim Buzdalov <mbuzdalov@gmail.com>
2025-01-07 09:16:30 +0000

    Change Main to only run one algorithm on one file one time.

--- src/main/java/com/github/mbuzdalov/tree4network/Main.java
+++ src/main/java/com/github/mbuzdalov/tree4network/Main.java
@@ -10,74 +10,121 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.Optional;
 
 public class Main {
-    private static final List<BestTreeAlgorithm> algorithms = List.of(
-            new BestMSTOverEdgeShuffle(),
-            new BestBSTOverRandomPermutations(),
-            new BestBSTOverAllPermutations(),
-            new SimpleLocalSearch<>(new BestMSTOverEdgeShuffle(), EdgeSwitchMutation.getInstance()),
-            new SimpleLocalSearch<>(new BestMSTOverEdgeShuffle(), SubtreeSwapMutation.getInstance()),
-            new SimpleLocalSearch<>(new BestMSTOverEdgeShuffle(), EdgeRandomRelinkMutation.getInstance()),
-            new SimpleLocalSearch<>(new BestMSTOverEdgeShuffle(), EdgeOptimalRelinkMutation.getInstance()),
-            new SimpleLocalSearch<>(new BestMSTOverEdgeShuffle(), RandomChoiceMutation.getInstance()),
-            new SimpleLocalSearch<>(new BestBSTOverRandomPermutations(), EdgeSwitchMutation.getInstance()),
-            new SimpleLocalSearch<>(new BestBSTOverRandomPermutations(), SubtreeSwapMutation.getInstance()),
-            new SimpleLocalSearch<>(new BestBSTOverRandomPermutations(), EdgeRandomRelinkMutation.getInstance()),
-            new SimpleLocalSearch<>(new BestBSTOverRandomPermutations(), EdgeOptimalRelinkMutation.getInstance()),
-            new SimpleLocalSearch<>(new BestBSTOverRandomPermutations(), RandomChoiceMutation.getInstance())
+    private record NamedAlgorithm(String name, BestTreeAlgorithm algorithm) {}
+    private record NamedGraph(String name, Graph graph) {}
+
+    private static final List<NamedAlgorithm> algorithms = List.of(
+            new NamedAlgorithm("MST", new BestMSTOverEdgeShuffle()),
+            new NamedAlgorithm("BST/rand", new BestBSTOverRandomPermutations()),
+            new NamedAlgorithm("BST/next", new BestBSTOverAllPermutations()),
+            new NamedAlgorithm("MST+switch", new SimpleLocalSearch<>(new BestMSTOverEdgeShuffle(), EdgeSwitchMutation.getInstance())),
+            new NamedAlgorithm("MST+subtree", new SimpleLocalSearch<>(new BestMSTOverEdgeShuffle(), SubtreeSwapMutation.getInstance())),
+            new NamedAlgorithm("MST+replaceR", new SimpleLocalSearch<>(new BestMSTOverEdgeShuffle(), EdgeRandomRelinkMutation.getInstance())),
+            new NamedAlgorithm("MST+replaceO", new SimpleLocalSearch<>(new BestMSTOverEdgeShuffle(), EdgeOptimalRelinkMutation.getInstance())),
+            new NamedAlgorithm("MST+random", new SimpleLocalSearch<>(new BestMSTOverEdgeShuffle(), RandomChoiceMutation.getInstance())),
+            new NamedAlgorithm("BST+switch", new SimpleLocalSearch<>(new BestBSTOverRandomPermutations(), EdgeSwitchMutation.getInstance())),
+            new NamedAlgorithm("BST+subtree", new SimpleLocalSearch<>(new BestBSTOverRandomPermutations(), SubtreeSwapMutation.getInstance())),
+            new NamedAlgorithm("BST+replaceR", new SimpleLocalSearch<>(new BestBSTOverRandomPermutations(), EdgeRandomRelinkMutation.getInstance())),
+            new NamedAlgorithm("BST+replaceO", new SimpleLocalSearch<>(new BestBSTOverRandomPermutations(), EdgeOptimalRelinkMutation.getInstance())),
+            new NamedAlgorithm("BST+random", new SimpleLocalSearch<>(new BestBSTOverRandomPermutations(), RandomChoiceMutation.getInstance()))
     );
 
-    private record Task(String graphName, Graph graph, int algoIndex, int runIndex, long timeLimitMillis, PrintWriter log) implements Runnable {
+    private record Task(NamedGraph graph, NamedAlgorithm algo, String runID, long timeLimitMillis, PrintWriter log) implements Runnable {
         private void writeFitness(long time, long fitness) {
-            synchronized (log) {
-                log.println(graphName + "," + algoIndex + "," + runIndex + "," + time + "," + fitness);
-            }
+            log.println(graph.name() + "," + algo.name() + "," + runID + "," + time + "," + fitness);
         }
 
         @Override
         public void run() {
             Timer timer = Timer.newFixedTimer(System.currentTimeMillis(), timeLimitMillis);
-            BestTreeAlgorithm algo = algorithms.get(algoIndex);
-            BestTreeAlgorithm.ExtendedResult result = algo.solve(graph, timer, this::writeFitness);
-            synchronized (System.out) {
-                System.out.print(graphName + "," + algoIndex + "," + runIndex + "," + timer.timeConsumedMillis() + ": ");
-                if (result.result() != null) {
-                    System.out.println(result.result().cost() + ", " + result.nQueries() + " queries");
-                } else {
-                    System.out.println("interrupted");
-                }
+            BestTreeAlgorithm.ExtendedResult result = algo.algorithm().solve(graph.graph(), timer, this::writeFitness);
+            System.out.print(graph.name() + "," + algo.name() + "," + runID + "," + timer.timeConsumedMillis() + ": ");
+            if (result.result() != null) {
+                System.out.println(result.result().cost() + ", " + result.nQueries() + " queries");
+            } else {
+                System.out.println("interrupted");
             }
         }
     }
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        File[] tests = new File("data").listFiles();
-        if (tests == null) {
-            System.err.println("Cannot retrieve the list of test files");
-            System.exit(1);
+    private static void usage() {
+        System.err.println("Usage: <algo> <file> <runID> <timeout> <fitness-log>, where:");
+        System.err.println("  <algo>         the name of the algorithm to run");
+        System.err.println("  <file>         the dataset to run the algorithm on");
+        System.err.println("  <runID>        a string that gets logged verbatim to indicate the run");
+        System.err.println("  <timeout>      the maximum runtime, in seconds");
+        System.err.println("  <fitness-log>  the file for fitness trajectory logging");
+        System.err.println("Available algorithms are: ");
+        for (int i = 0; i < algorithms.size(); ++i) {
+            System.err.print(i == 0 ? "  " : ", ");
+            System.err.print(algorithms.get(i).name());
         }
-        long timeLimitMillis = 1000L * Integer.parseInt(args[0]);
-        int nCPUs = Integer.parseInt(args[1]);
-        int nRuns = Integer.parseInt(args[2]);
+        System.err.println();
+        System.exit(1);
+    }
 
-        ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(nCPUs);
-        try (PrintWriter log = new PrintWriter(new FileWriter("log.csv"), true)) {
-            for (File test : tests) {
-                Graph graph = GraphFromCSV.fromGZippedFile(test);
-                for (int runIndex = 0; runIndex < nRuns; ++runIndex) {
-                    for (int algoIndex = 0; algoIndex < algorithms.size(); ++algoIndex) {
-                        executor.execute(new Task(test.getName(), graph, algoIndex, runIndex, timeLimitMillis, log));
-                    }
-                }
-            }
+    private static NamedAlgorithm getAlgorithm(String[] args) {
+        String algoName = args[0];
+        Optional<NamedAlgorithm> maybeAlgo = algorithms
+                .stream()
+                .filter(a -> a.name().equals(algoName))
+                .findFirst();
+        if (maybeAlgo.isEmpty()) {
+            System.err.println("Error: unknown algorithm '" + algoName + "'");
+            usage();
+            throw new IllegalStateException("System.exit(1) failed");
+        }
+        return maybeAlgo.get();
+    }
 
-            executor.shutdown();
-            while (!executor.awaitTermination(10, TimeUnit.DAYS)) {
-                System.out.println("Executor is still not terminated");
+    private static NamedGraph getGraph(String[] args) {
+        String fileName = args[1];
+        try {
+            File file = new File(fileName);
+            Graph g = GraphFromCSV.fromGZippedFile(file);
+            String name = file.getName();
+            name = name.substring(0, name.indexOf(".csv.gz"));
+            return new NamedGraph(name, g);
+        } catch (Throwable th) {
+            System.err.println("Error: cannot read graph '" + fileName + "'");
+            th.printStackTrace(System.err);
+            usage();
+            throw new IllegalStateException("System.exit(1) failed");
+        }
+    }
+
+    private static long getTimeout(String[] args) {
+        String timeout = args[3];
+        try {
+            long result = Long.parseLong(timeout);
+            if (result <= 0) {
+                System.err.println("Error: expected positive timeout, found '" + result + "'");
+                usage();
+                throw new IllegalStateException("System.exit(1) failed");
             }
+            return result;
+        } catch (NumberFormatException ex) {
+            System.err.println("Error: expected number as timeout, found '" + timeout + "'");
+            usage();
+            throw new IllegalStateException("System.exit(1) failed");
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        if (args.length != 5) {
+            usage();
+            return;
+        }
+
+        NamedAlgorithm algo = getAlgorithm(args);
+        NamedGraph graph = getGraph(args);
+        String runID = args[2];
+        long timeLimitMillis = getTimeout(args) * 1000;
+        try (PrintWriter log = new PrintWriter(new FileWriter(args[4], true))) {
+            new Task(graph, algo, runID, timeLimitMillis, log).run();
         }
     }
 }
