a0f93749c8eafb8b85ecfa119636d753ef0a7c85
Maxim Buzdalov <mbuzdalov@gmail.com>
2025-01-07 06:33:57 +0000

    Document the Combinatorics methods.

--- src/main/java/com/github/mbuzdalov/tree4network/util/Combinatorics.java
+++ src/main/java/com/github/mbuzdalov/tree4network/util/Combinatorics.java
@@ -5,6 +5,10 @@ import java.util.Random;
 public final class Combinatorics {
     private Combinatorics() {}
 
+    /**
+     * Fills the given array with the identity permutation, that is, [0, 1, ..., n - 1].
+     * @param permutation the array to be filled with the identity permutation.
+     */
     public static void fillIdentityPermutation(int[] permutation) {
         int n = permutation.length;
         for (int i = 0; i < n; ++i) {
@@ -12,6 +16,11 @@ public final class Combinatorics {
         }
     }
 
+    /**
+     * Fills the given array with a random permutation, using the supplied random number generator.
+     * @param permutation the array to be filled with a random permutation.
+     * @param random the random number generator to use.
+     */
     public static void fillRandomPermutation(int[] permutation, Random random) {
         int n = permutation.length;
         permutation[0] = 0;
@@ -22,6 +31,11 @@ public final class Combinatorics {
         }
     }
 
+    /**
+     * Fills the given array with an inverse of the given permutation.
+     * @param permutation the source permutation.
+     * @param inverse the array to be filled with the inverse of #permutation.
+     */
     public static void fillInverseOrder(int[] permutation, int[] inverse) {
         if (permutation.length != inverse.length) {
             throw new IllegalArgumentException("Array lengths are not equal");
@@ -31,15 +45,32 @@ public final class Combinatorics {
         }
     }
 
-    public static <T> void shufflePart(T[] edges, int from, int until, Random random) {
+    /**
+     * Shuffles a part of the given array using the supplied random number generator.
+     * @param array the array to be modified.
+     * @param from the first index of the array, inclusive, to be shuffled.
+     * @param until the last index of the array, exclusive, to be shuffled.
+     * @param random the random number generator to use.
+     * @param <T> the type of elements in the array.
+     */
+    public static <T> void shufflePart(T[] array, int from, int until, Random random) {
         for (int i = from + 1; i < until; ++i) {
             int j = random.nextInt(i - from + 1) + from;
-            T e = edges[i];
-            edges[i] = edges[j];
-            edges[j] = e;
+            T e = array[i];
+            array[i] = array[j];
+            array[j] = e;
         }
     }
 
+    /**
+     * <p>Given an array of distinct integers, produces the lexicographically next permutation
+     * and returns the minimum updated index.</p>
+     * <p>If the array is decreasing, reverses it and returns -1.</p>
+     *
+     * @param permutation the array containing a permutation,
+     *                    for which the lexicographically next permutation needs to be created.
+     * @return the minimum index that has changed, or -1 if the last permutation was turned into the first permutation.
+     */
     public static int nextPermutation(int[] permutation) {
         int n = permutation.length;
         for (int i = n - 2; i >= 0; --i) {
@@ -61,9 +92,18 @@ public final class Combinatorics {
                 return i;
             }
         }
+        fillIdentityPermutation(permutation);
         return -1;
     }
 
+    /**
+     * <p>Computes factorials to fit in 64-bit integers.</p>
+     * <p>For integers 0 to 20, inclusively, returns a factorial of the argument.</p>
+     * <p>For bigger integers, returns {@link Long#MAX_VALUE}.</p>
+     * <p>For negative integers, throws an {@link IllegalArgumentException}.</p>
+     * @param n the integer for which the factorial needs to be computed.
+     * @return the factorial, or {@link Long#MAX_VALUE} if the factorial is too large.
+     */
     public static long factorialOrMaxLong(int n) {
         if (n < 0) {
             throw new IllegalArgumentException("n is negative");
